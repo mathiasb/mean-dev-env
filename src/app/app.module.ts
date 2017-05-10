f21c4d9dcfd2d0838d92fb5ee47451be1ae7159d
Mathias Bergqvist <mathias.bergqvist@gmail.com>
2017-05-10 14:54:27 +0200

    added conf for Travis CI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './mainmenu.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent
   ],
   bootstrap: [ AppComponent ]
 })
