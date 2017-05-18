ad8cbb5b9b04286b97b24d7387c4f9fe081eeb73
Mathias Bergqvist <mathias.bergqvist@gmail.com>
2017-05-18 23:28:49 +0200

    using local mock data for development in launcher

--- ng-assets/app/web-api.service.ts
+++ ng-assets/app/web-api.service.ts
@@ -10,13 +10,15 @@ import { User } from './models';
 @Injectable()
 export class WebApiService {
   private paypal_url:string;
+  private base_url:string;
 
   constructor(private http:Http) {
+    this.base_url = (!/localhost/.test(document.location.host)) ? '/' : 'http://localhost:4001/'
     this.paypal_url = 'https://api.paypal.com';
   }
 
   getUsers(localUsers: User[]) {
-    this.get('/users').subscribe(
+    this.get('users').subscribe(
       users => {
         for (var u of users) {
           localUsers.push(u);
@@ -35,7 +37,7 @@ export class WebApiService {
   }
 
   private get(url: string): Observable<any> {
-    return this.http.get(url)
+    return this.http.get(this.base_url + url)
       .map((res: Response) => res.json())
       .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
   }
