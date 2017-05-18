5f9291c329c582787540297078d69ce86006120d
Mathias Bergqvist <mathias.bergqvist@gmail.com>
2017-05-18 15:15:42 +0200

    mock users through server route

--- serve/routes/app.js
+++ serve/routes/app.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var web_api = require('../webapi/api-handler');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('_index', { title: 'Zing Dashboard' });
+  res.render('_index', { title: 'My Dashboard' });
+});
+
+router.get('/users', function(req, res, next) {
+  var userList = web_api.getUsers();
+  res.send(userList);
+//  res.render('_index', { users: userList });
 });
 
 module.exports = router;
